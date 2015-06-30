848f4c5d87d86ad6005f2ed13d03201e9b3123bb
whiteday910 <whiteday910@gmail.com>
2015-06-30 17:52:48 +0900

    Initial Commit

---
+++ KoreanToThaiDic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KoreanToThaiDic
+//
+//  Created by jhkim on 2015. 6. 30..
+//  Copyright (c) 2015년 smallhouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
